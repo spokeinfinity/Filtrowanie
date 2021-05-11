32c673a25ca37436b9de877e734a59c373df313d
spokeinfinity <czelukpro@gmail.com>
2021-05-11 21:28:59 +0200

    Initial Commit

---
+++ Filtrowanie/Utils/CustomFilterAttributes.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace filters.Utils
+{
+    public class CustomFilterAttributes : ResultFilterAttribute
+    {
+        public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+        {
+            IPAddress IP = context.HttpContext.Connection.LocalIpAddress;
+            IPAddress IPv4 = IP.MapToIPv4();
+           /* List<string> hosts = new List<string>();
+            var temp = Dns.GetHostAddresses(Dns.GetHostName());
+            foreach (var _ in temp)
+            { hosts.Add(_.ToString()); }*/
+            var result = context.Result;
+              if (result is PageResult page)
+            {
+                page.ViewData["IP_Addressv4"] = IPv4;
+            }
+            await next();
+        }
+        
+
+    }
+}
---
+++ Filtrowanie/Utils/CustomPageFilter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Configuration;
+
+namespace filters.Utils
+{
+    public class CustomPageFilter { 
+      
+    }
+}
+
